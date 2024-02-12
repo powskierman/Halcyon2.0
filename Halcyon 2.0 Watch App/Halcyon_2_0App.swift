e0178351980630ecd48eef84e2a8e8e769ad1dff
powskierman <mlapoint99@hotmail.com>
2024-02-12 10:54:55 -1000

    Initial Commit

---
+++ Halcyon 2.0 Watch App/Halcyon_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Halcyon_2_0App.swift
+//  Halcyon 2.0 Watch App
+//
+//  Created by Michel Lapointe on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct Halcyon_2_0_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
