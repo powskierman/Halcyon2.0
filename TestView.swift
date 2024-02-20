9ac12d87825395a00f7a253ff4db47b1cbeb2937
powskierman <mlapoint99@hotmail.com>
2024-02-20 09:34:10 -1000

    View debugging commit

---
+++ TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  Halcyon 2.0 Watch App
+//
+//  Created by Michel Lapointe on 2024-02-20.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleView_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Simple Preview")
+    }
+}
